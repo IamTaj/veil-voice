d358e5f9c508fe620b0a8519dbe27398b7c956ec
IamTaj <night123fever@gmail.com>
2024-08-04 01:51:01 +0530

    fixed toggling of the accept message switch api issue

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -12,10 +12,6 @@ export async function GET(request: Request) {
   const _user: User = session?.user as User
 
   if (!session || !_user) {
-    // return Response.json(
-    //   { success: false, message: "Not authenticated" },
-    //   { status: 401 },
-    // )
     return ApiResponse(false, "Not authenticated", 401)
   }
   const userId = new mongoose.Types.ObjectId(_user._id)
@@ -28,26 +24,17 @@ export async function GET(request: Request) {
     ]).exec()
 
     if (!user || user.length === 0) {
-      // return Response.json(
-      //   { message: "User not found", success: false },
-      //   { status: 404 },
-      // )
       return ApiResponse(false, "User not found", 404)
     }
-
-    // return Response.json(
-    //   { messages: user[0].messages },
-    //   {
-    //     status: 200,
-    //   },
-    // )
-    return ApiResponse(true, user[0].userMessage, 200)
+    return ApiResponse(
+      true,
+      "User Message Fetched Successfully",
+      200,
+      user[0].userMessage,
+    )
   } catch (error) {
     console.error("An unexpected error occurred:", error)
-    // return Response.json(
-    //   { message: "Internal server error", success: false },
-    //   { status: 500 },
-    // )
+
     return ApiResponse(false, "Internal server error", 500)
   }
 }
