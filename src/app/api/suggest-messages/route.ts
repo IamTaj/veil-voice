400c5eae207b9ef70939a222f412b6a3901c1c80
IamTaj <night123fever@gmail.com>
2024-08-04 01:52:11 +0530

    added suggest message route

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,42 +1,25 @@
-import { OpenAI } from "openai"
-import { OpenAIStream, StreamingTextResponse } from "ai"
-import { NextRequest, NextResponse } from "next/server"
+import { OPENAI_KEY } from "@/lib/constant"
+import { ApiResponse } from "@/utils/ApiResponse"
+import { GoogleGenerativeAI } from "@google/generative-ai"
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-})
-
-export const runtime = "edge"
+const genAI = new GoogleGenerativeAI(OPENAI_KEY)
 
 export async function POST(request: Request) {
   try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be?|| What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-4",
-      max_tokens: 400,
-      stream: true,
-      messages: [{ role: "user", content: prompt }],
-    })
+    const response = await model.generateContent(prompt)
 
-    const stream = OpenAIStream(response)
-    return new StreamingTextResponse(stream)
-  } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = error
-      return NextResponse.json(
-        {
-          name,
-          status,
-          headers,
-          message,
-        },
-        { status },
-      )
-    } else {
-      console.log("An unexpected error occurred", error)
-      throw error
-    }
+    return ApiResponse(
+      true,
+      "AI Message Generated successfully",
+      201,
+      response?.response?.candidates?.[0]?.content?.parts?.[0]?.text,
+    )
+  } catch (err: any) {
+    return ApiResponse(true, err, 500)
   }
 }
